6470cf988b4d04b6f9499a779b79b365fba07a9b
Dvan143 <dvanish445@yandex.ru>
2024-11-10 00:20:14 +0300

    Added requesting by api from OpenWeather

--- src/main/java/org/example/weather/MainController.java
+++ src/main/java/org/example/weather/MainController.java
@@ -1,12 +1,24 @@
 package org.example.weather;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class MainController {
     @GetMapping
     public String index() {
+        return "index";
+    }
+    public String index(@RequestParam String lat, @RequestParam String lon, Model model) {
+        // Sending request to get Weather
+        final String uri = "https://api.open-meteo.com/v1/forecast?latitude=" + lat + "&longitude=" + lon + "&daily=weather_code,temperature_2m_max,temperature_2m_min&wind_speed_unit=kn&forecast_days=3";
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(uri, String.class);
+
+        model.addAttribute("result", result);
         return "index";
     }
 }
--- src/main/resources/static/code.js
+++ src/main/resources/static/code.js
@@ -1,3 +1,7 @@
-// navigator.geolocation.getCurrentPosition(function (position) {
-//     console.log(position.coords.latitude, position.coords.longitude);
-// });
+var lat = 0
+var lon = 0
+navigator.geolocation.getCurrentPosition(function (position) {
+    lat = position.coords.latitude;
+    lon = position.coords.longitude;
+    if(window.location.href == "") window.location.href = "?latitude=" + lat  + "longitude=" + lon;
+});
