0e7d6cf62fda6e2cb4eb366965307b68679db4df
Dvan143 <dvanish445@yandex.ru>
2024-11-10 18:11:00 +0300

    small changes

--- src/main/java/org/example/weather/MainController.java
+++ src/main/java/org/example/weather/MainController.java
@@ -1,6 +1,5 @@
 package org.example.weather;
 
-import com.google.gson.Gson;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,8 +13,6 @@ import java.util.Map;
 public class MainController {
     @GetMapping("/")
     public String index(Model model) {
-        // model.addAttribute("result", result);
-        model.addAttribute("gool", "eee");
         return "index";
     }
     @GetMapping("/getWeather")
@@ -25,19 +22,43 @@ public class MainController {
         RestTemplate restTemplate = new RestTemplate();
         String result = restTemplate.getForObject(uri, String.class);
         setWeather();
-        int weatherForecast = Integer.parseInt(result.split(",\"")[15].split(",")[0]);
-        // "weather_code":[95,95,2] //
-//        Gson gson = new Gson();
-//        String json = gson.fromJson(result, String.class);
-        model.addAttribute("debug", getWeatherByNum(weatherForecast));
+        String weatherForecast = result;
+        int[] goli = new int[3];
+        for (int i = 0; i < weatherForecast.length(); i++) {
+            char[] temp = weatherForecast.toCharArray();
+            int j = 0 ;
+            if (temp[i] != '[' || temp[i] != ']'){
+                if(temp[i] == ',') j++;
+                goli[j] = temp[i];
+            }
+        }
+        // "weather_code":[95,95,2]
+       /* {"latitude":14.0,"longitude":88.0,"generationtime_ms":0.04494190216064453,"utc_offset_seconds":0,"timezone":"GMT","timezone_abbreviation":"GMT","elevation":0.0,"daily_units":{"time":"iso8601","weather_code":"wmo code","temperature_2m_max":"°C","temperature_2m_min":"°C","wind_speed_10m_max":"km/h"},"daily":{"time":["2024-11-10","2024-11-11","2024-11-12"],"weather_code":[95,95,3],"temperature_2m_max":[28.9,28.9,29.1],"temperature_2m_min":[28.0,28.0,28.3],"wind_speed_10m_max":[20.0,22.7,27.6]}}
+        Weather now:
+        95,95,3*/
         // 18 strings
+        String debug = result.split(",\"")[15];
+        String temp[] = debug.split(":");
+        String temp2 = temp[1];
+        temp2 = temp2.replace("[","");
+        temp2 = temp2.replace("]","");
+        String svo2[] = temp2.split(",");
+        int[] temp3 = new int[3];
+        for (int i = 0; i < 3; i++) {
+            temp3[i] = Integer.parseInt(svo2[i]);
+        }
 
-        // String debug = result.split(",\"")[15];
-        // model.addAttribute("debug", debug);
+        model.addAttribute("dayToday", getWeatherByNum(temp3[0]));
+        model.addAttribute("dayAfterDay", getWeatherByNum(temp3[1]));
+        model.addAttribute("dayAfterTwoDays", getWeatherByNum(temp3[2]));
         model.addAttribute("result", result);
+
         return "index";
     }
     Map<Integer,String> weatherList = new HashMap<>();
+    public String getWeatherByNum(int num){
+        return weatherList.get(num);
+    }
     public void setWeather(){
         weatherList.put(0,"Clear sky");
         weatherList.put(1,"Mainly clear");
@@ -67,8 +88,5 @@ public class MainController {
         weatherList.put(95,"Light thunderstorm");
         weatherList.put(96,"Moderate thunderstorm");
         weatherList.put(99,"High thunderstorm");
-    }
-    public String getWeatherByNum(int num){
-        return weatherList.get(num);
     }
 }
--- src/main/resources/static/code.js
+++ src/main/resources/static/code.js
@@ -1,8 +1,9 @@
 var lat = 0
-var lon = 0
-navigator.geolocation.getCurrentPosition(function (position) {
-    lat = position.coords.latitude;
-    lon = position.coords.longitude;
-    if(window.location.href == "") window.location.href = "/getWeather?lat=" + lat  + "lon=" + lon;
-    console.log(lat,lon);
-});
+    var lon = 0
+    navigator.geolocation.getCurrentPosition(function (position) {
+        lat = position.coords.latitude;
+        lon = position.coords.longitude;
+        if(window.location.href === "http://localhost:8080/") window.location.href = "/getWeather?lat=" + lat  + "&lon=" + lon;
+        console.log(lat,lon);
+        console.log(window.location.href);
+    });
