cf4d2092012c3e0feb3c05670837f2f5797ffda6
Dvan143 <dvanish445@yandex.ru>
2025-04-25 15:01:04 +0300

    Removed support getting ip by push-notification.

--- src/main/java/org/example/weather/MainController.java
+++ src/main/java/org/example/weather/MainController.java
@@ -1,9 +1,9 @@
 package org.example.weather;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
@@ -12,26 +12,35 @@ import java.util.Map;
 @Controller
 public class MainController {
     @GetMapping("/")
-    public String index(Model model) {
-        return "index";
-    }
-    @GetMapping("/getWeather")
-    public String index(@RequestParam String lat, @RequestParam String lon, Model model) {
-
-        // Sending request to get Weather
-        String uri = "https://api.open-meteo.com/v1/forecast?latitude=" + lat +"&longitude=" + lon + "&daily=weather_code,temperature_2m_max,temperature_2m_min,wind_speed_10m_max&forecast_days=3";
+    public String index(Model model, HttpServletRequest request) {
         RestTemplate restTemplate = new RestTemplate();
+
+        // Getting IP
+        String ClientIp = request.getRemoteAddr();
+
+        // Getting Longitude and Latitude by gotten IP
+        String uri = "http://www.geoplugin.net/json.gp?ip=" + ClientIp;
+        String json3 = restTemplate.getForObject(uri, String.class);
+        String lat = json3.split("latitude\":\"")[1].split("\"")[0];
+        String lon = json3.split("longitude\":\"")[1].split("\"")[0];
+
+        // Getting weather
+        uri = "https://api.open-meteo.com/v1/forecast?latitude=" + lat +"&longitude=" + lon + "&daily=weather_code,temperature_2m_max,temperature_2m_min,wind_speed_10m_max&forecast_days=3";
         String json = restTemplate.getForObject(uri, String.class);
         setWeather();
         String tempStr = json.split("weather_code\":\\[")[1].split("]")[0];
         String[] tempArrString = tempStr.split(",");
 
+        // Getting address
         uri = "https://nominatim.openstreetmap.org/reverse?format=json&lat="+ lat +"&lon=" + lon;
         String json2 = restTemplate.getForObject(uri, String.class);
 
-        String countryName = json2.split("country\":\"")[1].split("\"")[0];
-        String regionName = json2.split("state\":\"")[1].split("\"")[0];
-        String cityName = json2.split("town\":\"")[1].split("\"")[0];
+        String countryName = "";
+        if (json2.contains("country")) countryName = json2.split("country\":\"")[1].split("\"")[0];
+        String regionName = "";
+        if(json2.contains("state")) regionName = json2.split("state\":\"")[1].split("\"")[0];
+        String cityName = "";
+        if (json2.contains("town")) cityName = json2.split("town\":\"")[1].split("\"")[0];
 
         model.addAttribute("imageName", getNameOfWeatherByNum(tempArrString[0]));
 
@@ -47,6 +56,7 @@ public class MainController {
 
         return "index";
     }
+
     Map<String, String[]> weatherList = new HashMap<>();
     public String getWeatherByNum(String num){
         return weatherList.get(num)[0];
---
+++ src/main/java/org/example/weather/ServletInitializer.java
@@ -0,0 +1,11 @@
+package org.example.weather;
+
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WeatherApplication.class);
+    }
+}
